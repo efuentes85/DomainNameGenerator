f191676c0b14c48d6ced4017ee624ebf160b440f
Eduardo Fuentes Velasco <efuentes85@gmail.com>
2019-09-06 14:42:44 +0000

    Adding Flask server and HTML code

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var k;
 for (i = 0; i < pronoun.length; i++) {
   	for(j = 0; j < adj.length ; j++){
           for(k = 0 ; k < noun.length; k++){
-         domain += pronoun[i]+adj[j]+noun[k]+".com"+"<br>";      }
+         domain += pronoun[i]+adj[j]+noun[k]+".com"+"<br>"; }
     }
 }
 document.getElementById("demo").innerHTML = domain;
