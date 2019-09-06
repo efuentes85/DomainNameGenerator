31495316bc65ea6243b2b39864006ded80d79960
Eduardo Fuentes Velasco <efuentes85@gmail.com>
2019-09-06 00:46:32 +0000

    update 5 Sept

---
+++ main.js
@@ -0,0 +1,19 @@
+function DomainNameGenerator(){
+var pronoun = ['the','our'];
+var adj = ['great', 'big' ];
+var noun = ['jogger','racoon'];
+var domain="";
+
+var i;
+var j;
+var k;
+
+for (i = 0; i < pronoun.length; i++) {
+  	for(j = 0; j < adj.length ; j++){
+          for(k = 0 ; k < noun.length; k++){
+         domain += pronoun[i]+adj[j]+noun[k]+".com"+"<br>";      }
+    }
+}
+document.getElementById("demo").innerHTML = domain;
+}
+DomainNameGenerator();
